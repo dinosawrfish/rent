8d827c4b1f1b35509a030524a84a5a33f15120bf
dinosawrfish <dinosawrfish@gmail.com>
2024-07-31 17:50:13 -0700

    conditionals

--- experiments.js
+++ experiments.js
@@ -20,3 +20,14 @@ console.log(sentenceWithTemplate);
 
 // boolean
 const lightIsOn = true;
+
+// conditionals
+const friendsAtYourParty = 10;
+
+if (friendsAtYourParty === 0) {
+  console.log("Cool, now I have a lot of nachos to myself.");
+} else if (friendsAtYourParty <= 4) {
+  console.log("Perfect amount to play some Mario Kart.");
+} else {
+  console.log("Wooooo turn on the dance music!");
+}
