71f09896930ce9fe553e74df92d5fc1358c30272
dinosawrfish <dinosawrfish@gmail.com>
2024-08-01 15:36:28 -0700

    arrays

--- experiments.js
+++ experiments.js
@@ -178,3 +178,48 @@ const me = {
   };
 
   console.log(meTwo.getAddress());
+
+  //arrays
+  const daysOfTheWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  console.log(daysOfTheWeek);
+  console.log(daysOfTheWeek[0]);
+  console.log(daysOfTheWeek.join(" | "))
+
+  const courses = [
+    { teacher: "Will Sentance", course: "JavaScript: The Hard Parts" },
+    { teacher: "Sarah Drasner", course: "Intro to Vue" },
+    { teacher: "Brian Holt", course: "Complete Intro to React" },
+    { teacher: "Steve Kinney", course: "Build Your Own Programming Language" },
+    { teacher: "Scott Moss", course: "Intro to Node.js" },
+  ];
+
+  courses.push({ teacher: "Jen Kramer", course: "Getting Started with CSS" });
+  console.log(courses);
+  courses[2] = { teacher: "Brian Holt", course: "Complete Intro to Databases" };
+  console.log(courses);
+
+  const cities = [
+    "Seattle",
+    "San Francisco",
+    "Salt Lake City",
+    "Amsterdam",
+    "Hong Kong",
+  ];
+
+  // method 1
+  for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i]);
+  }
+
+  // method 2
+  cities.forEach(function (city) {
+    console.log(city);
+  });
