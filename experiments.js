66ed94e7b6c4a64d38754b1cd7509d803cc7c517
dinosawrfish <dinosawrfish@gmail.com>
2024-07-31 17:37:57 -0700

    strings and boolean

--- experiments.js
+++ experiments.js
@@ -2,3 +2,21 @@ const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+
+// strings
+const myName = "Brian Holt";
+console.log(myName);
+
+const firstName = "Brian";
+const lastName = "Holt";
+
+// string concat
+const sentence = "Hello " + firstName + " " + lastName + "! How are you!?";
+// template strings
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you!?`;
+
+console.log(sentence);
+console.log(sentenceWithTemplate);
+
+// boolean
+const lightIsOn = true;
